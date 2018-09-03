b52ed961716234f1297bd5ab1227b150c480a99d
Nathan Gaberel <nathan@gnab.fr>
2018-09-03 01:16:15 +0100

    Proper resolving.

--- src/app.js
+++ src/app.js
@@ -4,33 +4,38 @@ import Koa from "koa"
 import bodyParser from "koa-bodyparser"
 
 import render from "./render"
-import resolve from "./resolve"
+import buildResolver from "./resolve"
 
-const app = new Koa()
+async function app() {
+  const app = new Koa()
+  const resolve = await buildResolver("./build/documents")
 
-app.use(bodyParser())
+  app.use(bodyParser())
 
-app.use(async (ctx, next) => {
-  try {
-    ctx.document = await resolve(ctx.request.url)
-    await next()
-  } catch (error) {
-    ctx.status = 404
-    ctx.body = "document not found"
-  }
-})
-
-app.use(async ctx => {
-  const {
-    document: { filename, Component },
-    request: { body: props },
-  } = ctx
-
-  ctx.set({
-    "Content-Disposition": `attachment; filename="${filename}"`,
-    "Content-Type": "application/pdf",
+  app.use(async (ctx, next) => {
+    try {
+      ctx.document = resolve(ctx.request.url)
+      await next()
+    } catch (error) {
+      ctx.status = 404
+      ctx.body = "document not found"
+    }
   })
-  ctx.response.body = await render(Component, props)
-})
 
-app.listen(80)
+  app.use(async ctx => {
+    const {
+      document: { filename, Component },
+      request: { body: props },
+    } = ctx
+
+    ctx.set({
+      "Content-Disposition": `attachment; filename="${filename}"`,
+      "Content-Type": "application/pdf",
+    })
+    ctx.response.body = await render(Component, props)
+  })
+
+  app.listen(80)
+}
+
+app()
--- src/resolve.js
+++ src/resolve.js
@@ -1,13 +1,41 @@
-import { Test } from "./documents"
+import path from "path"
+import walk from "walk"
 
-export const buildResolver = path => {
-  return key => {
-    return {
-      filename: "test.pdf",
-      Component: Test,
-    }
-    // throw "nope"
-  }
+const allowedExtensions = [".js", ".jsx"]
+
+function buildResolverIndex(root) {
+  root = path.resolve(root)
+  const walker = walk.walk(root)
+  const index = {}
+
+  return new Promise((resolve, reject) => {
+    walker.on("file", (currenRoot, stats, next) => {
+      const extension = path.extname(stats.name)
+      const basename = path.basename(stats.name, extension)
+
+      if (allowedExtensions.includes(extension)) {
+        const filePath = path.resolve(currenRoot, stats.name)
+        const relative = path.relative(root, filePath)
+        const relativeDir = path.dirname(relative)
+        const trimed = path.join(relativeDir, basename)
+
+        index[`/${trimed}`] = {
+          filename: `${basename}.pdf`,
+          Component: require("./" + path.relative(__dirname, filePath)).default,
+        }
+      }
+
+      next()
+    })
+
+    walker.on("errors", reject)
+    walker.on("end", () => resolve(index))
+  })
 }
 
-export default buildResolver("./documents")
+async function buildResolver(root) {
+  const index = await buildResolverIndex(root)
+  return key => index[key]
+}
+
+export default buildResolver
